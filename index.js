76c1d2c514e62298aff344222273f93cb58b3e86
ShripriyaR <shripriyaravi@gmail.com>
2023-12-09 21:41:52 +0530

    env update

--- index.js
+++ index.js
@@ -25,6 +25,8 @@ const PORT = process.env.PORT || 5000
 
 const DATABASE_URL = process.env.CONNECTION_URL
 
+mongoose.set('strictQuery', false);
+
 mongoose.connect( DATABASE_URL , {useNewUrlParser: true, useUnifiedTopology: true})
     .then(() => app.listen(PORT, () => {console.log(`server running on port ${PORT}`)}))
     .catch((err) => console.log(err.message))
